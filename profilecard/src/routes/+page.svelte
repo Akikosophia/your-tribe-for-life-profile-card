0f8e0ec99f86df5a0a05c84242f534cd7589adc1
Akikosophia <144008863+Akikosophia@users.noreply.github.com>
2024-09-17 12:57:36 +0200

    Npm install svelte & directus
    Installeren van Svelte en Directus, Data ophalen.

---
+++ profilecard/src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let data
+
+  function checkAvatarImage (avatar) {
+    if (avatar.includes('https')) {
+      return true
+    } 
+
+    return false
+  }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<main>
+  <h1>Squad 1D</h1>
+
+  <ul>
+    {#each data.persons as person}
+      <li>
+        <a href="/{person.id}">
+          <span class="avatar">
+            {#if person.avatar.length > 0 && checkAvatarImage (person.avatar)}
+              <img src="{person.avatar}" alt="Avatar van {person.name}">
+            {:else if person.avatar.length > 0 || checkAvatarImage (person.avatar)}
+              {person.avatar}
+            {:else}
+              <img src="https://robohash.org/mail@ashallendesign.co.uk" alt="Avatar van {person.name}">
+            {/if}
+          </span>
+
+          {person.name}
+        </a>
+      </li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+
+</style>
---
+++ profilecard/src/routes/[id]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let person;
+  export let error;
+</script>
+
+{#if error}
+  <p>{error}</p>
+{:else if person}
+  <h1>{person.name}</h1>
+  <p>{person.bio}</p>
+  <!-- Voeg hier andere velden van 'person' toe -->
+{:else}
+  <p>Geen gegevens beschikbaar</p>
+{/if}
+  
